fb5ead21e4707fa4d337c4ea2319fcc4f978c4df
MementoTrumpo <98746183+MementoTrumpo@users.noreply.github.com>
2024-01-19 13:43:00 +0300

    Adding some functions
    Добавление функции для решения системы и корректировка некоторых файлов

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-from Equation import LastEquation, DifferentialEquationSystem
+import matplotlib.pyplot as plt
+from Solve import Solver, LastEquation
 from flet import (
     # Layout controls
     Page, Container, Column, ResponsiveRow, Card,
@@ -14,6 +14,25 @@ from flet import (
 )
 import numpy as np # Нужно импортить только нужное
 
+# Функция для решения системы 2 порядка
+def func(t, x, y, last_equation: LastEquation):
+    if(y == "1"):
+        y_param = 1
+    else:
+        y_param = math.sin(t)
+
+    if(last_equation.n == 2):
+        dxdt = np.zeros(2)
+        dxdt[0] = x[1]
+        dxdt[1] = (1 / last_equation.a_2) * (last_equation.b_0*y_param - last_equation.a_1*x[1] - last_equation.a_0*x[0])
+        return dxdt
+    else:
+        dxdt = np.zeros(3)
+        dxdt[0] = x[1]
+        dxdt[1] = x[2]
+        dxdt[2] = (1 / last_equation.a_3) * (last_equation.b_0*y_param - last_equation.a_2*x[2] - last_equation.a_1*x[1] - last_equation.a_0*x[0])
+        return dxdt
+
 def main(page: Page):
     page.title = "Лабораторная работа №1"
     page.window_width = 900
@@ -27,7 +46,7 @@ def main(page: Page):
     def validateData():
         problemFields = []
         # Проверка на ввод всех необходимых данных в нужные поля
-        global fields 
+        global fields
         fields = {'Порядок': modelOrder,
                 'a0': a0,
                 'a1': a1,
@@ -39,7 +58,7 @@ def main(page: Page):
                 'x__0': x__0,
                 'y(t)': y_dropdown,
                 'Интервал t': interval}
-        
+
         if not modelOrder.value:
             problemFields.append(modelOrder)
 
@@ -54,17 +73,17 @@ def main(page: Page):
             for field in fieldsToCheck:
                 if not fields[field].value:
                     problemFields.append(fields[field])
-        
+
         if not y_dropdown.value:
             problemFields.append(y_dropdown)
         if not interval.value:
             problemFields.append(interval)
 
         if len(problemFields) != 0:
-            # Всем пробленным полям добавляется иконка ошибки 
+            # Всем пробленным полям добавляется иконка ошибки
             for field in problemFields:
                 field.icon = icons.ERROR_OUTLINE
-            
+
             # Создание диалогового окна с ошибкой
             errorDialog = AlertDialog(
                 modal=True,
@@ -84,6 +103,52 @@ def main(page: Page):
     def sendData(e):
         # Валидация данных на предмет заполненности полей
         validateData()
+        # Выбор параметров для последнего уравнения
+        lastEquation = LastEquation(int(modelOrder.value))
+        if(lastEquation.n == 3):
+            lastEquation.init_order_3(
+                                    float(a0.value),
+                                    float(a1.value),
+                                    float(a2.value),
+                                    float(a3.value),
+                                    float(b0.value))
+        else:
+            lastEquation.init_order_2(
+                                    float(a0.value),
+                                    float(a1.value),
+                                    float(a2.value),
+                                    float(b0.value))
+        # Параметр y (либо y(t) = 1, либо y(t) = sin(t))
+        y_parametr = y_dropdown.value
+        # Начальные значения x(0), x'(0), x''(0) для 2 и 3 порядка модели
+        if(lastEquation.n == 3):
+            y0 = [float(x0.value), float(x_0.value), float(x__0.value)]
+        else:
+            y0 = [float(x0.value), float(x_0.value)]
+
+        # Интервал интегрирования
+        t_span = [0, int(interval.value)]
+        # Составление системы для решения
+        system = Solver(func, y0, t_span, y_parametr, lastEquation)
+        # Решение системы
+        solution = system.solve()
+        # Вывод графиков
+        if(lastEquation.n == 3):
+            plt.plot(solution.t, solution.y[0], label='x(t)')
+            plt.plot(solution.t, solution.y[1], label="x'(t)")
+            plt.plot(solution.t, solution.y[2], label="x''(t)")
+            plt.xlabel('t')
+            plt.ylabel('x')
+            plt.legend()
+            plt.show()
+        else:
+            plt.plot(solution.t, solution.y[0], label='x(t)')
+            plt.plot(solution.t, solution.y[1], label="x'(t)")
+            plt.xlabel('t')
+            plt.ylabel('x')
+            plt.legend()
+            plt.show()
+
         page.update()
 
     def orderChanged(e):
@@ -114,7 +179,7 @@ def main(page: Page):
                 filed.value = ""
                 filed.disabled = True
         page.update()
-    
+
     # Удаляет иконку ошибки при наведении на поле
     def removeIcon(e):
         if e.control.icon is not None:
@@ -130,9 +195,9 @@ def main(page: Page):
         dropdown.Option("2"),
         dropdown.Option("3")],
         col={"md": 12},
-        on_change=orderChanged, 
+        on_change=orderChanged,
         on_focus=removeIcon)
-    
+
     a0 = TextField(label="a0", disabled=True,  keyboard_type=KeyboardType.NUMBER, input_filter=inp_filter, on_focus=removeIcon, col={"md": 4})
     a1 = TextField(label="a1", disabled=True,  keyboard_type=KeyboardType.NUMBER, input_filter=inp_filter, on_focus=removeIcon, col={"md": 4})
     a2 = TextField(label="a2", disabled=True,  keyboard_type=KeyboardType.NUMBER, input_filter=inp_filter, on_focus=removeIcon, col={"md": 4})
@@ -148,8 +213,8 @@ def main(page: Page):
     title3 = Text("Выбор параметров", weight="bold", size=20)
     y_dropdown = Dropdown(label="y(t)", options=[
         dropdown.Option("1"),
-        dropdown.Option("sin(t)")], 
-        width=200, 
+        dropdown.Option("sin(t)")],
+        width=200,
         on_focus=removeIcon,
         col={"md": 6}
     )
@@ -168,14 +233,6 @@ def main(page: Page):
         dxdt[0] = x[1]
         dxdt[1] = (1 / last_equation.a_2) * (last_equation.b_0*y - last_equation.a_1*x[1] - last_equation.a_0*x[0])
 
-    # if(modelOrder == 3):
-    #     last_equation = LastEquation(3, a0, a1, a2, a3, b0)
-    # else:
-    #     last_equation = LastEquation(2, a0, a1, a2, None, b0)
-
-    # x0 = [x0, x_0, x__0]  # начальные условия
-    # t_span = [0, 20]  # интервал интегрирования
-
     sendDataButton = ElevatedButton(text="Отправить", on_click=sendData, icon=icons.SEND, height=50)
 
     # Карточка с вводом информации о порядке модели
@@ -188,7 +245,7 @@ def main(page: Page):
                     ResponsiveRow(
                         [a0, a1, a2]
                     ),
-                    
+
                     ResponsiveRow(
                         [a3, b0]
                     )
@@ -198,7 +255,7 @@ def main(page: Page):
             padding=20
         )
     )
-    
+
     # Карточка с вводом информации о начальных условиях
     card2 = Card(
         Container(
@@ -254,7 +311,7 @@ def main(page: Page):
         page.dialog = eggDialog
         eggDialog.open = True
         page.update()
-    
+
     page.add(card1, card2, card3, button, TextButton(on_click=openEgg, icon=icons.INFO_OUTLINE))
 
 # Запускаем приложение
